cc3a3724d1e3407c1347d88b034cfd6c3d364aae
Muhammad-Saeed-Ahmad-SL <saeed.ahmad@solutionsloft.com>
2024-01-08 17:54:31 +0500

    Create event form structure using react-hook-form with zod validation and write custom dropdown and description area

---
+++ components/shared/DropDown/index.tsx
@@ -0,0 +1,74 @@
+import React, { startTransition, useState } from 'react';
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { ICategory } from '@/lib/database/models/category.model';
+import { Input } from '@/components/ui/input';
+
+type DropDownProps = {
+  value?: string;
+  onChangeHandler?: () => void;
+};
+
+const DropDown = ({ value, onChangeHandler }: DropDownProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  const [categories, setCatgories] = useState<ICategory[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  const [newCategory, setNewCatgory] = useState('');
+
+  const handleAddCategory = () => {};
+  return (
+    <>
+      <Select onValueChange={onChangeHandler} defaultValue={value}>
+        <SelectTrigger className='select-field'>
+          <SelectValue placeholder='Category' />
+        </SelectTrigger>
+        <SelectContent>
+          {categories.length > 0 &&
+            categories.map(category => (
+              <SelectItem key={category._id} value={category._id} className='select-item p-regular-14'>
+                {category.name}
+              </SelectItem>
+            ))}
+          <AlertDialog>
+            <AlertDialogTrigger
+              className='p-medium-14 flex w-full rounded-sm py-3 pl-8
+             text-primary-500 hover:bg-primary-50 focus:text-primary-500'
+            >
+              Open
+            </AlertDialogTrigger>
+            <AlertDialogContent className='bg-white'>
+              <AlertDialogHeader>
+                <AlertDialogTitle>New Category</AlertDialogTitle>
+                <AlertDialogDescription>
+                  <Input
+                    type='text'
+                    placeholder='Category Name'
+                    className='input-field mt-3'
+                    onChange={e => setNewCatgory(e.target.value)}
+                  />
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => startTransition(handleAddCategory)}>Add</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </SelectContent>
+      </Select>
+    </>
+  );
+};
+
+export default DropDown;
