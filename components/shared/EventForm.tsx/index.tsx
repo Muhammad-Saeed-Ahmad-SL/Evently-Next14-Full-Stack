d992a6b1720c0af1e1b9ba691050127c44d8a1a4
Muhammad-Saeed-Ahmad-SL <saeed.ahmad@solutionsloft.com>
2024-01-10 23:48:37 +0500

    Event form submission and action. category action of event also

--- components/shared/EventForm.tsx/index.tsx
+++ components/shared/EventForm.tsx/index.tsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import Image from 'next/image';
 import DatePicker from 'react-datepicker';
+import { useRouter } from 'next/navigation';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -16,6 +17,8 @@ import { eventFormSchema } from '@/lib/validator';
 import { eventDefaultValues } from '@/constants';
 import { Textarea } from '@/components/ui/textarea';
 import { Checkbox } from '@/components/ui/checkbox';
+import { useUploadThing } from '@/lib/uploadthing';
+import { createEvent } from '@/lib/actions/events.actions';
 
 import DropDown from '../DropDown';
 import { FileUploader } from '../FileUploader';
@@ -25,22 +28,49 @@ type EventFormProps = {
   type: 'Create' | 'Update';
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 const EventForm = ({ userId, type }: EventFormProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   const [files, setFiles] = useState<File[]>([]);
-
   const initialValues = eventDefaultValues;
+  const router = useRouter();
+  const { startUpload } = useUploadThing('imageUploader');
 
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues,
   });
 
-  function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    // eslint-disable-next-line no-console
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
+
+    if (files.length > 0) {
+      const uploadedImages = await startUpload(files);
+
+      if (!uploadedImages) {
+        return;
+      }
+
+      uploadedImageUrl = uploadedImages[0].url;
+    }
+
+    if (type === 'Create') {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl: uploadedImageUrl },
+          userId,
+          path: '/profile',
+        });
+
+        if (newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
+    }
   }
+  // 3:6:09
   return (
     <>
       <Form {...form}>
